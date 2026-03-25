989db5ba32784776ff80c1786a494da45e731704
Saii Neeli <sairoy1224@gmail.com>
2026-03-25 10:51:20 +0530

    Add files via upload

--- program.py
+++ program.py
@@ -1,14 +1,9 @@
-// Neo4j Cypher Queries
+import pandas as pd
 
-MATCH (p:Person)
-WHERE p.age > 26
-RETURN p.name, p.age;
+data = pd.read_csv("students.csv")
 
-MATCH (:Person)-[r:FRIENDS_WITH]->(:Person)
-RETURN COUNT(r);
-
-MATCH (a:Person {name:'Alice'})-[:FRIENDS_WITH]->(friends)
-RETURN friends.name;
-
-MATCH (a)-[:FRIENDS_WITH]->(b)-[:FRIENDS_WITH]->(c)
-RETURN a.name, c.name;
+print(data.head())
+print(data.shape)
+print(data.columns)
+print(data.describe())
+print(data.isnull().sum())
